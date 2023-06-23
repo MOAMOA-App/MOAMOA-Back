490b6dd7a1c3f4089afb74698089227ac12e43c6
kim-se <kimse990507@gmail.com>
2023-06-23 16:16:37 +0900

    Feat : 미사용 entity 관련 파일 삭제

--- src/main/java/org/zerock/moamoa/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package org.zerock.moamoa.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import org.zerock.moamoa.domain.entity.Category;
-
-@Repository
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
