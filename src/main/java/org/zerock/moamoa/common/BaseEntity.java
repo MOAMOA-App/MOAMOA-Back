37fb6ab3f44f681e69b79ff9a482a7b577368421
kim-se <kimse990507@gmail.com>
2023-06-27 15:33:34 +0900

    untrack .DS_Store

---
+++ src/main/java/org/zerock/moamoa/common/BaseEntity.java
@@ -0,0 +1,29 @@
+package org.zerock.moamoa.common;
+
+import java.time.Instant;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Data;
+
+@Data
+@MappedSuperclass
+//다른 엔티티들이 상속받기 위한 기반 클래스
+//공유 속성을 모아두는 상위 엔티티임을 나타내는 어노테이션
+@EntityListeners(AuditingEntityListener.class)
+//Entity의 변화를 지켜보는 클래스 정의
+public class BaseEntity {
+	@CreatedDate
+	@Column(updatable = false)
+	private Instant createdAt;
+
+	@LastModifiedDate
+	@Column(updatable = true)
+	private Instant updatedAt;
+}
