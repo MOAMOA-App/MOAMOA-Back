3d8c1cef50ded4bb6cc53b75612b38615dffb1d5
tasty13 <bodri0625@gmail.com>
2023-11-15 20:25:15 +0900

    Fix: 게시글 삭제 pid Param으로 전달, 그외 유저 관련 오류 수정

--- src/main/java/org/zerock/moamoa/domain/DTO/user/UserSignupRequest.java
+++ src/main/java/org/zerock/moamoa/domain/DTO/user/UserSignupRequest.java
@@ -22,11 +22,11 @@ public class UserSignupRequest {
 
 
     @Builder
-    public UserSignupRequest(String email, String password) {
+    public UserSignupRequest(String nick, String email, String password) {
+        this.nick = nick;
 //        this.name = name;
         this.email = email;
         if (password != null) this.password = password;
         this.activate = true;
-
     }
 }
